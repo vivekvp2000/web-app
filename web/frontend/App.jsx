1a23e4a52b91779cb525522a7938c9dc014cd2cf
vivekvp2000 <vivekpayal999@gmail.com>
2025-03-03 17:27:44 +0530

    add config in app

--- web/frontend/App.jsx
+++ web/frontend/App.jsx
@@ -4,15 +4,21 @@ import { NavMenu } from "@shopify/app-bridge-react";
 import Routes from "./Routes";
 import './assets/css/app.css'
 import { QueryProvider, PolarisProvider } from "./components";
+import { useEffect } from "react";
 
 export default function App() {
-  // Any .tsx or .jsx files in /pages will become a route
-  // See documentation for <Routes /> for more info
   const pages = import.meta.glob("./pages/**/!(*.test.[jt]sx)*.([jt]sx)", {
     eager: true,
   });
   const { t } = useTranslation();
-
+  
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const shop = urlParams.get("shop");
+    if (shop) {
+      window.location.href = `https://shopify-app-backend.amirait.in/public/auth?shop=${shop}`;
+    }
+  }, []);
   return (
     <PolarisProvider>
       <BrowserRouter>
